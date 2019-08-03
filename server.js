78d0b8f93b23fab4c30ee37330221d7b5f4facff
Becky Ghita <ghita109@gmail.com>
2019-08-03 14:00:09 -0700

    add server to heroku

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-require("dotenv").config();
+const PORT = process.env.PORT || 7777;
 const controllers = require("./controllers/controllers.js");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./routes/routes.js");
-
-const PORT = process.env.PORT || 7777;
 const app = express();
+require("dotenv").config();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
